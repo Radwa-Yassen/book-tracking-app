ab26f251a9091e62a070ccbe33a0dbd26fb94f9d
Radwa-Yassen <radwayassen99@gmail.com>
2022-12-05 09:44:04 +0100

    remove commented code

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,8 @@
 import { Link } from "react-router-dom";
-//import { getAll, update } from "../store/book-actions";
 import classes from "./HomePage.module.css";
-//import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import BooksShelves from "../components/BooksShelves";
-//import { RootState, selectbooks, useAppDispatch } from "../store";
-
-//const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 const HomePage: React.FC = (props) => {
-  //const dispatch = useAppDispatch();
-  //const state = useTypedSelector(selectbooks);
-
-  //console.log(state[0]);
-
-  //   const WTRBooks= books.map((book:Book)=>{
-  // return book.shelf==="wantToRead";
-  //   });
-
-  //   const RBooks= books.map((book:Book)=>{
-  // return book.shelf==="Read";
-  //   });
-
-  //   const moveBookHandler = (book: Book, shelf: string) => {
-  //  update(book,shelf).then((res=> console.log(res)));
-  //   };
 
   return (
     <div className={classes.page}>
